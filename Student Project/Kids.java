85b3da367ba2286d44f5f1cb276faa3593689c7d
rayansb1 <136233403+rayansb1@users.noreply.github.com>
2023-06-11 13:18:04 +0300

    Add files via upload

---
+++ Student Project/Kids.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public class Kids implements Grading{
+
+	private String name;
+	private int nba; //Number of Answers
+
+	public Kids(){
+	name = " ";
+	nba= 0;
+	}
+
+	public Kids(String nn, int bb){
+	name = nn;
+	nba = bb;
+	}
+
+
+	public void computeGrade() {
+	
+		if (nba >= 100) 
+		System.out.println("You win");
+		
+			else
+		System.out.println("You lost");
+		
+
+	}
+}
